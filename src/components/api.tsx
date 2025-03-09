0707256eb2bee004c1114fdfc1c5fe82bb2cf06d
evanbacon <bacon@expo.io>
2025-03-09 18:00:28 -0500

    refactor: remove old index.html and update API integration in components

--- src/components/api.tsx
+++ src/components/api.tsx
@@ -3,8 +3,10 @@
 import { createSpotifyAPI } from "@/components/spotify/spotify-actions";
 import * as serverActions from "@/components/spotify/spotify-server-actions";
 
+const api = createSpotifyAPI(serverActions);
+console.log("Provider", api);
+
 // Wrapping to ensure the auth context is available on the server without needing to manually pass to each function.
-export const {
-  Provider: SpotifyActionsProvider,
-  useSpotify: useSpotifyActions,
-} = createSpotifyAPI(serverActions);
+const { Provider: SpotifyActionsProvider, useSpotify: useSpotifyActions } = api;
+
+export { SpotifyActionsProvider, useSpotifyActions };
