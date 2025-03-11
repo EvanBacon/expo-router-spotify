d743f2477ed4bfd598789c7637672ff57f98d3c2
evanbacon <bacon@expo.io>
2025-03-11 13:39:58 -0500

    refactor: update app configuration, enhance Spotify token management, and add new dependencies

--- src/lib/spotify-auth/auth-server-actions.tsx
+++ src/lib/spotify-auth/auth-server-actions.tsx
@@ -12,59 +12,51 @@ const {
   SPOTIFY_CLIENT_SECRET: clientSecret,
 } = process.env;
 
-export async function exchangeAuthCodeAsync(props: {
-  code: string;
-  redirectUri: string;
-}): Promise<SpotifyCodeExchangeResponse> {
+const authorization =
+  "Basic " + Buffer.from(clientId + ":" + clientSecret).toString("base64");
+
+async function makeTokenCallAsync(searchParams: URLSearchParams) {
   const body = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
     headers: {
       "content-type": "application/x-www-form-urlencoded",
-      Authorization:
-        "Basic " +
-        Buffer.from(clientId + ":" + clientSecret).toString("base64"),
+      Authorization: authorization,
     },
-    body: new URLSearchParams({
-      code: props.code,
-      redirect_uri: props.redirectUri,
-      grant_type: "authorization_code",
-    }).toString(),
+    body: searchParams,
   }).then((res) => res.json());
+  console.log("[SPOTIFY] token:", body);
 
-  console.log("[SPOTIFY] requestAccessToken:", body);
   const response = SpotifyCodeExchangeResponseSchema.parse(body);
   if ("expires_in" in response) {
     // Set the expiration time to the current time plus the number of seconds until it expires.
     response.expires_in = Date.now() + response.expires_in * 1000;
   }
-
   return response;
+}
+
+export async function exchangeAuthCodeAsync(props: {
+  code: string;
+  redirectUri: string;
+}): Promise<SpotifyCodeExchangeResponse> {
+  return await makeTokenCallAsync(
+    new URLSearchParams({
+      grant_type: "authorization_code",
+      code: props.code,
+      redirect_uri: props.redirectUri,
+    })
+  );
 }
 
 export async function refreshTokenAsync(
   refreshToken: string
 ): Promise<SpotifyCodeExchangeResponse> {
-  const body = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      Authorization:
-        "Basic " +
-        Buffer.from(clientId + ":" + clientSecret).toString("base64"),
-    },
-    body: new URLSearchParams({
+  const response = await makeTokenCallAsync(
+    new URLSearchParams({
       grant_type: "refresh_token",
-      refresh_token: refreshToken,
       client_id: clientId!,
-    }),
-  }).then((res) => res.json());
-
-  console.log("[SPOTIFY] refreshToken:", body);
-  const response = SpotifyCodeExchangeResponseSchema.parse(body);
-  if ("expires_in" in response) {
-    // Set the expiration time to the current time plus the number of seconds until it expires.
-    response.expires_in = Date.now() + response.expires_in * 1000;
-  }
+      refresh_token: refreshToken,
+    })
+  );
   response.refresh_token ??= refreshToken;
 
   return response;
